1c342a46484d85c797ee104d9fb2703f6dbed39e
lofienjoyer <25750692+lofi-enjoyer@users.noreply.github.com>
2023-03-25 12:15:41 +0000

    Project creation

---
+++ quiet/discovery-service/src/test/java/me/lofienjoyer/quiet/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lofienjoyer.quiet.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
